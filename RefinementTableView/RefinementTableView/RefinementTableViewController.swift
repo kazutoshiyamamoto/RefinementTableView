c511cf09cfd698c6e54e1cba7cd327ba14df0acd
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-06 20:04:53 +0900

    絞り込みメニュー画面の実装を追加

--- RefinementTableView/RefinementTableView/RefinementTableViewController.swift
+++ RefinementTableView/RefinementTableView/RefinementTableViewController.swift
@@ -12,8 +12,30 @@ class RefinementTableViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    private let refinementMenuTitle = ["カテゴリ"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
     }
 }
+
+extension RefinementTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.refinementMenuTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        cell.RefinementConditionTitle.text = self.refinementMenuTitle[indexPath.row]
+        return cell
+    }
+}
+
+extension RefinementTableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
