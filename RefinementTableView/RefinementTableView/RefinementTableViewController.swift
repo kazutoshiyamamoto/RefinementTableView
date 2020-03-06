707cdcfdd0f71cd53799d478e0c8c0c683672d72
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-06 19:31:12 +0900

    UITableViewをカスタムクラスと関連づける

--- RefinementTableView/RefinementTableView/RefinementTableViewController.swift
+++ RefinementTableView/RefinementTableView/RefinementTableViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class RefinementTableViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 }
