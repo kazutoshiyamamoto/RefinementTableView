c5a012bec5a9607326693c5082a3f2699c326732
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-06 19:22:48 +0900

    絞り込みView用のカスタムクラスを追加

---
+++ RefinementTableView/RefinementTableView/RefinementTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RefinementTableViewController.swift
+//  RefinementTableView
+//
+//  Created by home on 2020/03/06.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class RefinementTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
