5c02ceb321fa121dc76a1b39623f363dfb6cb8de
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-10 18:05:54 +0900

    カテゴリ選択画面用Storyboardでカスタムクラスの設定を追加

---
+++ RefinementTableView/RefinementTableView/CategoryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryViewController.swift
+//  RefinementTableView
+//
+//  Created by home on 2020/03/10.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
