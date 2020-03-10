9960ae92f6f848a325c19a09554f33d53ba013cc
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-10 18:39:08 +0900

    カテゴリ一覧表示処理を追加

--- RefinementTableView/RefinementTableView/CategoryViewController.swift
+++ RefinementTableView/RefinementTableView/CategoryViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    private let categoryName = ["トップス", "ボトムス", "アウター"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.tableFooterView = UIView()
+    }
+}
 
+extension CategoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.categoryName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = self.categoryName[indexPath.row]
+        return cell
+    }
+}
+
+extension CategoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
     }
 }
