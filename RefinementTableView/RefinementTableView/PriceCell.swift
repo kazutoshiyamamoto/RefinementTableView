e8a4f41d80b74d0e151639e9a8886338c0e10029
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-11 23:55:48 +0900

    絞り込みメニューに表示する価格設定用セルを追加

---
+++ RefinementTableView/RefinementTableView/PriceCell.swift
@@ -0,0 +1,24 @@
+//
+//  PriceCell.swift
+//  RefinementTableView
+//
+//  Created by home on 2020/03/11.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class PriceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
