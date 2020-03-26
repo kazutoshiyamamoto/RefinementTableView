66e9ff71314f5b2abdfe356b32200b8cce9d4f0a
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-26 21:57:25 +0900

    検索/クリアボタンを設置するセルを追加

---
+++ RefinementTableView/RefinementTableView/RunCell.swift
@@ -0,0 +1,24 @@
+//
+//  RunCell.swift
+//  RefinementTableView
+//
+//  Created by home on 2020/03/26.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class RunCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
