78638b84ebbf0fa24f57fbee50a36547dede7a6e
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-30 20:41:29 +0900

    絞り込み画面:価格入力時のキーボードに完了ボタンを追加

--- RefinementTableView/RefinementTableView/PriceCell.swift
+++ RefinementTableView/RefinementTableView/PriceCell.swift
@@ -15,7 +15,8 @@ class PriceCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.setDoneToolbar(field: self.minPrice)
+        self.setDoneToolbar(field: self.maxPrice)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -24,4 +25,24 @@ class PriceCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    private func setDoneToolbar(field: UITextField) {
+        // サイズ指定しないとレイアウト制約でコンフリクトする
+        // https://forums.developer.apple.com/thread/121474
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+
+        doneToolbar.items = [
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "完了", style: UIBarButtonItem.Style.done, target: self, action: #selector(PriceCell.done))
+        ]
+
+        doneToolbar.sizeToFit()
+        field.inputAccessoryView = doneToolbar
+    }
+    
+    @objc private func done() {
+        self.minPrice.resignFirstResponder()
+        self.maxPrice.resignFirstResponder()
+    }
 }
+
+
