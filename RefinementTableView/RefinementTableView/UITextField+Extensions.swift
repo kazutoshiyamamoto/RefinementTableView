6f2ff740e440a703475e70004266a61832309880
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-30 18:40:28 +0900

    テキストフィールドの拡張を実装するファイルを追加

---
+++ RefinementTableView/RefinementTableView/UITextField+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UITextField+Extensions.swift
+//  RefinementTableView
+//
+//  Created by home on 2020/03/30.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    
+}
