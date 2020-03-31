f69a177492f6785d894993e9a53a1472d6b96286
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-31 16:40:06 +0900

    絞り込み画面:価格入力セルに数字を打ち込むたびに絞り込み画面へ通知を飛ばす

--- RefinementTableView/RefinementTableView/PriceCell.swift
+++ RefinementTableView/RefinementTableView/PriceCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PriceCell: UITableViewCell {
-
+    
     @IBOutlet weak var minPrice: UITextField!
     @IBOutlet weak var maxPrice: UITextField!
     
@@ -18,28 +18,40 @@ class PriceCell: UITableViewCell {
         self.setDoneToolbar(field: self.minPrice)
         self.setDoneToolbar(field: self.maxPrice)
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
 
-        // Configure the view for the selected state
+    // Editing Changedで呼び出される
+    @IBAction func editMinPrice(_ sender: UITextField) {
+        let center = NotificationCenter.default
+        center.post(name: .setMinPrice, object: nil, userInfo: ["minPrice": self.minPrice.text ?? ""])
+    }
+    
+    // Editing Changedで呼び出される
+    @IBAction func editMaxPrice(_ sender: UITextField) {
+        let center = NotificationCenter.default
+        center.post(name: .setMaxPrice, object: nil, userInfo: ["maxPrice": self.maxPrice.text ?? ""])
     }
     
     private func setDoneToolbar(field: UITextField) {
         // サイズ指定しないとレイアウト制約でコンフリクトする
-        // https://forums.developer.apple.com/thread/121474
+        // 参考:https://forums.developer.apple.com/thread/121474
         let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-
+        
+        // 完了ボタン選択時にキーボードを閉じるメソッドを呼び出す
         doneToolbar.items = [
             UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil),
-            UIBarButtonItem(title: "完了", style: UIBarButtonItem.Style.done, target: self, action: #selector(PriceCell.done))
+            UIBarButtonItem(title: "完了", style: UIBarButtonItem.Style.done, target: self, action: #selector(PriceCell.tappedDoneButton))
         ]
-
+        
         doneToolbar.sizeToFit()
         field.inputAccessoryView = doneToolbar
     }
     
-    @objc private func done() {
+    // キーボードを閉じる
+    @objc private func tappedDoneButton() {
         self.minPrice.resignFirstResponder()
         self.maxPrice.resignFirstResponder()
     }
