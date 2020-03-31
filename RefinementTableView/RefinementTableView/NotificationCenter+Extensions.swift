d8b781b31c919bc7fd971cc15f232b63e3cb307b
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-03-31 16:38:18 +0900

    Notificationの拡張を追加

---
+++ RefinementTableView/RefinementTableView/NotificationCenter+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationCenter+Extensions.swift
+//  RefinementTableView
+//
+//  Created by home on 2020/03/31.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let setMinPrice = Notification.Name("setMinPrice")
+    static let setMaxPrice = Notification.Name("setMaxPrice")
+}
