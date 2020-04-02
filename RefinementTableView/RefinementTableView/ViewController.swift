d520d855660ae482eddfd560f2a898d4c3c81b9b
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-02 22:08:45 +0900

    TOP画面のラベルをカスタムクラスと関連づける

--- RefinementTableView/RefinementTableView/ViewController.swift
+++ RefinementTableView/RefinementTableView/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var minPrice: UILabel!
+    @IBOutlet weak var maxPrice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
