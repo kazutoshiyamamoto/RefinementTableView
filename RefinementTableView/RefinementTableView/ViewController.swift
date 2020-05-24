e03ace58c92dbcd9eacfc748340c993df7df545f
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-05-24 16:28:29 +0900

    UISearchBarをNavigationControllerに埋め込む

--- RefinementTableView/RefinementTableView/ViewController.swift
+++ RefinementTableView/RefinementTableView/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     
     private var searchController: UISearchController!
     
+    // テスト
+    var searchBar: UISearchBar!
+    
     @IBOutlet weak var category: UILabel!
     @IBOutlet weak var minPrice: UILabel!
     @IBOutlet weak var maxPrice: UILabel!
@@ -25,14 +28,29 @@ class ViewController: UIViewController {
         let center = NotificationCenter.default
         center.addObserver(self, selector: #selector(type(of: self).setCondition(notification:)), name: .setCondition, object: nil)
         
-        self.searchController = UISearchController(searchResultsController: nil)
-        self.searchController.searchBar.placeholder = "何かお探しですか"
-        self.searchController.searchBar.delegate = self
-        // 検索中に元のコンテンツをグレーアウトしないようにする
-        self.searchController.obscuresBackgroundDuringPresentation = false
-        self.navigationItem.searchController = self.searchController
-        self.navigationItem.hidesSearchBarWhenScrolling = false
+//        self.searchController = UISearchController(searchResultsController: nil)
+//        self.searchController.searchBar.placeholder = "何かお探しですか"
+//        self.searchController.searchBar.delegate = self
+//        // 検索中に元のコンテンツをグレーアウトしないようにする
+//        self.searchController.obscuresBackgroundDuringPresentation = false
+//        self.navigationItem.searchController = self.searchController
+//        self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        self.setupSearchBar()
     }
+    
+    func setupSearchBar() {
+          if let navigationBarFrame = navigationController?.navigationBar.bounds {
+              let searchBar: UISearchBar = UISearchBar(frame: navigationBarFrame)
+              searchBar.delegate = self
+              searchBar.placeholder = "何かお探しですか"
+//              searchBar.tintColor = UIColor.gray
+//              searchBar.keyboardType = UIKeyboardType.default
+              navigationItem.titleView = searchBar
+              navigationItem.titleView?.frame = searchBar.frame
+              self.searchBar = searchBar
+          }
+      }
     
     // 絞り込み画面で検索実行すると呼ばれる
     @objc func setCondition(notification: NSNotification?) {
@@ -52,6 +70,9 @@ class ViewController: UIViewController {
 extension ViewController: UISearchBarDelegate {
     // 検索バー選択時に検索メニュー画面を表示
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        // キャンセルボタンを表示
+        self.searchBar.showsCancelButton = true
+        
         UIView.animate(withDuration: 0.3, animations: {
             self.searchMenuView.alpha = 1.0
         })
@@ -61,6 +82,11 @@ extension ViewController: UISearchBarDelegate {
     
     // 検索バーのキャンセルボタンを選択時に検索メニューを非表示にする
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        // キャンセルボタンを非表示に変更
+        self.searchBar.showsCancelButton = false
+        // キーボードを非表示に変更
+        self.searchBar.resignFirstResponder()
+        
         UIView.animate(withDuration: 0.3, animations: {
             self.searchMenuView.alpha = 0.0
         })
